69ffb68499072304da69218b9eefeb154e0d2aba
Bora Kaplan <hb.kpln@gmail.com>
2018-01-16 23:36:15 +0300

    Added action builder for user controllers.

--- app/actions/AuthenticatedAction.scala
+++ app/actions/AuthenticatedAction.scala
@@ -1,16 +1,36 @@
 package actions
 
+import java.time.ZonedDateTime
 import javax.inject.Inject
 
+import auth.Token
+import io.igl.jwt.Sub
+import play.api.Logger
+import play.api.libs.json._
 import play.api.mvc._
+import play.api.mvc.Results._
 
 import scala.concurrent.{ExecutionContext, Future}
-
 
 class AuthenticatedAction @Inject()(parser: BodyParsers.Default)
   (implicit ec: ExecutionContext) extends ActionBuilderImpl(parser) {
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]) = {
-    // TODO: Implement authentication check, get the account from token.
-    block(request)
+    val userId = for {
+      token <- request.headers.get("JWT")
+      jwt <- Token.validate(token)
+      sub <- jwt.getClaim[Sub]
+    } yield sub.value.toLong
+
+    userId match {
+      case Some(id) =>
+        Logger.info(s"[${readableDate(ZonedDateTime.now())}] - Successful authentication for user: $id")
+        block(request)
+      case _ =>
+        Logger.info(s"[${readableDate(ZonedDateTime.now())}] - Authentication error for request: ${request.body} ${request.headers}")
+        Future.successful(Forbidden(Json.obj("error" -> "Authentication Error")))
+    }
   }
+
+  def readableDate(date: ZonedDateTime) =
+    s"${date.getYear}-${date.getMonthValue}-${date.getDayOfMonth} ${date.getHour}:${date.getMinute}"
 }
--- app/auth/Token.scala
+++ app/auth/Token.scala
@@ -4,6 +4,7 @@ import io.igl.jwt._
 import models.Account
 import util.Logger
 import java.time.ZonedDateTime
+
 import play.api.libs.json._
 
 case class UserRole(value: String) extends ClaimValue {
@@ -18,10 +19,19 @@ object UserRole extends (String => UserRole) with ClaimField {
 }
 
 object Token extends Logger {
-  def generate(account: Account): String = {
+  def generate(account: Account): String =
     new DecodedJwt(
       Seq(Alg(Algorithm.HS256), Typ("JWT")),
       Seq(Sub(s"${account.id}"), UserRole(account.userType), Exp(ZonedDateTime.now().plusWeeks(2).toEpochSecond))
     ).encodedAndSigned(Security.tokenKey)
-  }
+
+
+  def validate(token: String): Option[Jwt] =
+    DecodedJwt.validateEncodedJwt(
+      token,                     // An encoded jwt as a string
+      Security.tokenKey,         // The key to validate the signature against
+      Algorithm.HS256,           // The algorithm we require
+      Set(Typ),                  // The set of headers we require (excluding alg)
+      Set(Sub, UserRole, Exp),   // The set of claims we require
+    ).toOption
 }
