3871af8ac764dcd59567c4daefc74cf1bdecbcab
Bora Kaplan <hb.kpln@gmail.com>
2018-01-17 21:57:27 +0300

    Updated date method for logging.

--- app/actions/AuthenticatedAction.scala
+++ app/actions/AuthenticatedAction.scala
@@ -17,6 +17,8 @@ class UserRequest[A](val userId: Long, request: Request[A]) extends WrappedReque
 class AuthenticatedAction @Inject()(bodyParser: BodyParsers.Default)
   (implicit ec: ExecutionContext) extends ActionBuilder[UserRequest, AnyContent] {
   def invokeBlock[A](request: Request[A], block: UserRequest[A] => Future[Result]) = {
+    implicit lazy val now = ZonedDateTime.now()
+
     val userId = for {
       token <- request.headers.get("JWT")
       jwt <- Token.validate(token)
@@ -25,15 +27,15 @@ class AuthenticatedAction @Inject()(bodyParser: BodyParsers.Default)
 
     userId match {
       case Some(id) =>
-        Logger.info(s"[${readableDate(ZonedDateTime.now())}] - Successful authentication for user: $id")
+        Logger.info(s"[$readableDate] - Successful authentication for user: $id")
         block(new UserRequest(id, request))
       case _ =>
-        Logger.info(s"[${readableDate(ZonedDateTime.now())}] - Authentication error for request: ${request.body} ${request.headers}")
+        Logger.info(s"[$readableDate] - Authentication error for request: ${request.body} ${request.headers}")
         Future.successful(Forbidden(Json.obj("error" -> "Authentication Error")))
     }
   }
 
-  def readableDate(date: ZonedDateTime) =
+  def readableDate(implicit date: ZonedDateTime) =
     s"${date.getYear}-${date.getMonthValue}-${date.getDayOfMonth} ${date.getHour}:${date.getMinute}"
 
   override def parser: BodyParser[AnyContent] = bodyParser
