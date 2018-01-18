cd04c4432ba155baccbf1184071bb281a1b2034d
Bora Kaplan <kaplanbora@protonmail.com>
2018-01-18 20:10:20 +0300

    Redesigned all models.

--- app/controllers/CategoryController.scala
+++ app/controllers/CategoryController.scala
@@ -17,30 +17,18 @@ class CategoryController @Inject()
 
   implicit val categoryFormFormat = Json.format[CategoryForm]
 
-
-  /**
-    * Get a category via its id.
-    *
-    * @param id Id of the category
-    * @return Category object
-    */
   def get(id: Long) = Action.async { implicit request =>
     repo.get(id).map {
       case Some(category) => Ok(Json.toJson(category))
-      case None => NotFound(Json.obj("message" -> "Category not found."))
+      case None => NotFound(Json.obj("error" -> "Category not found."))
     }
   }
 
-  /**
-    * Create a new category.
-    *
-    * @return Created category object
-    */
   def create = Action(parse.json).async { implicit request =>
     val result = request.body.validate[CategoryForm]
     result.fold(
       errors => {
-        Future.successful(BadRequest(Json.obj("message" -> JsError.toJson(errors))))
+        Future.successful(BadRequest(Json.obj("error" -> JsError.toJson(errors))))
       },
       category => {
         repo.create(category.name)
@@ -49,21 +37,10 @@ class CategoryController @Inject()
     )
   }
 
-  /**
-    * List all categories.
-    *
-    * @return Sequence of categories
-    */
   def list = Action.async { implicit request =>
     repo.list().map(x => Ok(Json.toJson(x)))
   }
 
-  /**
-    * Update a category.
-    *
-    * @param id Id of the category
-    * @return Id of the category
-    */
   def update(id: Long) = Action(parse.json).async { implicit request =>
     val result = request.body.validate[CategoryForm]
     result.fold(
--- queries/create.sql
+++ queries/create.sql
@@ -1,137 +1,199 @@
+DROP TABLE "Conversation";
+DROP TABLE "Message";
+DROP TABLE "Report";
+DROP TABLE "Review";
+DROP TABLE "Transaction";
+DROP TABLE "Lesson";
+DROP TABLE "Category";
 DROP TABLE "Account";
 
-CREATE TABLE "Account" (
-  "id"            BIGSERIAL,
-  "email"         VARCHAR(60)  NOT NULL,
-  "password"      VARCHAR(256) NOT NULL,
-  "first_name"    VARCHAR(40)  NOT NULL,
-  "last_name"     VARCHAR(20)  NOT NULL,
-  "creation_date" TIMESTAMP    NOT NULL,
-  "user_type"     VARCHAR(10) DEFAULT 'student',
-  CONSTRAINT "accountPK" PRIMARY KEY ("id"),
-  CONSTRAINT "emailUQ" UNIQUE ("email")
+CREATE TABLE "user_credentials" (
+  "id"            BIGSERIAL PRIMARY KEY,
+  "email"         TEXT      NOT NULL UNIQUE,
+  "password"      TEXT      NOT NULL,
+  "creation_date" TIMESTAMP NOT NULL,
+  "user_type"     TEXT      NOT NULL
 );
 
-CREATE TABLE "Category" (
-  "id"   BIGSERIAL,
-  "name" VARCHAR(100) NOT NULL,
-  CONSTRAINT "categoryPK" PRIMARY KEY ("id"),
-  CONSTRAINT "nameUQ" UNIQUE ("name")
-);
-
-CREATE TABLE "Conversation" (
-  "id"          BIGSERIAL,
-  "account1_id" BIGINT NOT NULL,
-  "account2_id" BIGINT NOT NULL,
-  CONSTRAINT "conversationPK" PRIMARY KEY ("id"),
-  CONSTRAINT "accountsUQ" UNIQUE ("account1_id", "account2_id"),
-  CONSTRAINT "conversationAccountFK1" FOREIGN KEY ("account1_id")
-  REFERENCES "Account" ("id")
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE,
-  CONSTRAINT "conversationAccountFK2" FOREIGN KEY ("account2_id")
-  REFERENCES "Account" ("id")
-  ON DELETE NO ACTION
+CREATE TABLE "instructor_profiles" (
+  "id"          BIGSERIAL PRIMARY KEY,
+  "first_name"  TEXT,
+  "last_name"   TEXT,
+  "description" TEXT,
+  "occupation"  TEXT,
+  "image_link"  TEXT,
+  "hourly_rate" DOUBLE PRECISION,
+  "video_link"  TEXT,
+  "user_id"     BIGINT NOT NULL UNIQUE,
+  CONSTRAINT "profilesCredentialsFK" FOREIGN KEY ("user_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE CASCADE
   ON UPDATE CASCADE
 );
 
-CREATE TABLE "Lesson" (
-  "id"           BIGSERIAL,
-  "teacher_id"   BIGINT           NOT NULL,
-  "category_id"  BIGINT           NOT NULL,
-  "name"         VARCHAR(200)     NOT NULL,
-  "price"        DOUBLE PRECISION NOT NULL,
-  "creationDate" TIMESTAMP        NOT NULL,
-  "description"  TEXT,
-  "is_active"    BOOLEAN DEFAULT TRUE,
-  CONSTRAINT "lessonPK" PRIMARY KEY ("id"),
-  CONSTRAINT "priceCHK" CHECK ("price" > 0 AND "price" < 1000),
-  CONSTRAINT "lessonUQ" UNIQUE ("teacher_id", "name"),
-  CONSTRAINT "lessonAccountFK" FOREIGN KEY ("teacher_id")
-  REFERENCES "Account" ("id")
+CREATE TABLE "instructor_topics" (
+  "id"            BIGSERIAL PRIMARY KEY,
+  "instructor_id" BIGINT NOT NULL,
+  "topic_id"      BIGINT NOT NULL,
+  CONSTRAINT "instopicsUQ" UNIQUE ("instructor_id", "topic_id"),
+  CONSTRAINT "instopicsFK1" FOREIGN KEY ("instructor_id")
+  REFERENCES "user_credentials" ("id")
   ON DELETE CASCADE
   ON UPDATE CASCADE,
-  CONSTRAINT "lessonCategoryFK" FOREIGN KEY ("category_id")
-  REFERENCES "Category" ("id")
-  ON DELETE RESTRICT
+  CONSTRAINT "instopicsFK2" FOREIGN KEY ("topic_id")
+  REFERENCES "topics" ("id")
+  ON DELETE CASCADE
   ON UPDATE CASCADE
 );
 
-CREATE TABLE "Message" (
-  "id"              BIGSERIAL,
+CREATE TABLE "trainee_profiles" (
+  "id"         BIGSERIAL PRIMARY KEY,
+  "first_name" TEXT,
+  "last_name"  TEXT,
+  "image_link" TEXT,
+  "user_id"    BIGINT NOT NULL UNIQUE,
+  CONSTRAINT "profilesCredentialsFK" FOREIGN KEY ("user_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE "topics" (
+  "id"        BIGSERIAL PRIMARY KEY,
+  "name"      TEXT   NOT NULL UNIQUE,
+  "parent_id" BIGINT NOT NULL,
+  CONSTRAINT "topicsTopicsFK" FOREIGN KEY ("parent_id")
+  REFERENCES "topics" ("id")
+  ON DELETE CASCADE
+  ON UPDATE CASCADE
+);
+
+
+CREATE TABLE "conversations" (
+  "id"       BIGSERIAL PRIMARY KEY,
+  "user1_id" BIGINT NOT NULL,
+  "user2_id" BIGINT NOT NULL,
+  CONSTRAINT "accountsUQ" UNIQUE ("user1_id", "user2_id"),
+  CONSTRAINT "conversationsCredentialsFK1" FOREIGN KEY ("user1_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+  CONSTRAINT "conversationsCredentialsFK2" FOREIGN KEY ("user2_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE "messages" (
+  "id"              BIGSERIAL PRIMARY KEY,
   "sender_id"       BIGINT    NOT NULL,
   "receiver_id"     BIGINT    NOT NULL,
   "conversation_id" BIGINT    NOT NULL,
   "body"            TEXT,
   "date"            TIMESTAMP NOT NULL,
-  CONSTRAINT "messagePK" PRIMARY KEY ("id"),
   CONSTRAINT "messageAccountFK1" FOREIGN KEY ("sender_id")
-  REFERENCES "Account" ("id")
+  REFERENCES "user_credentials" ("id")
   ON DELETE NO ACTION
   ON UPDATE CASCADE,
   CONSTRAINT "messageAccountFK2" FOREIGN KEY ("receiver_id")
-  REFERENCES "Account" ("id")
+  REFERENCES "user_credentials" ("id")
   ON DELETE NO ACTION
   ON UPDATE CASCADE,
   CONSTRAINT "messageConversationFK" FOREIGN KEY ("conversation_id")
-  REFERENCES "Conversation" ("id")
+  REFERENCES "user_credentials" ("id")
   ON DELETE NO ACTION
   ON UPDATE CASCADE
 );
 
-CREATE TABLE "Report" (
-  "id"          BIGSERIAL,
-  "lesson_id"   BIGINT       NOT NULL,
-  "student_id"  BIGINT       NOT NULL,
-  "title"       VARCHAR(200) NOT NULL,
-  "description" TEXT         NOT NULL,
-  "is_resolved" BOOLEAN DEFAULT FALSE,
-  "date"        TIMESTAMP    NOT NULL,
-  CONSTRAINT "reportPK" PRIMARY KEY ("id"),
-  CONSTRAINT "reportLessonFK" FOREIGN KEY ("lesson_id")
-  REFERENCES "Lesson" ("id")
-  ON DELETE RESTRICT
+CREATE TABLE "lessons" (
+  "id"            BIGSERIAL PRIMARY KEY,
+  "instructor_id" BIGINT           NOT NULL,
+  "topic_id"      BIGINT           NOT NULL,
+  "name"          TEXT             NOT NULL,
+  "price"         DOUBLE PRECISION NOT NULL,
+  "creation_date" TIMESTAMP        NOT NULL,
+  "image_link"    TEXT,
+  "video_link"    TEXT,
+  "description"   TEXT,
+  "is_active"     BOOLEAN DEFAULT TRUE,
+  CONSTRAINT "priceCHK" CHECK ("price" > 0 AND "price" < 1000),
+  CONSTRAINT "lessonUQ" UNIQUE ("instructor_id", "name"),
+  CONSTRAINT "lessonsCredentialsFK" FOREIGN KEY ("instructor_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE CASCADE
   ON UPDATE CASCADE,
-  CONSTRAINT "reportAccountFK" FOREIGN KEY ("student_id")
-  REFERENCES "Account" ("id")
+  CONSTRAINT "lessonsTopicsFK" FOREIGN KEY ("topic_id")
+  REFERENCES "topics" ("id")
   ON DELETE RESTRICT
   ON UPDATE CASCADE
 );
 
-CREATE TABLE "Review" (
-  "id"         BIGSERIAL,
-  "lesson_id"  BIGINT           NOT NULL,
-  "student_id" BIGINT           NOT NULL,
+CREATE TABLE "sessions" (
+  "id"            BIGSERIAL PRIMARY KEY,
+  "name"          TEXT      NOT NULL,
+  "trainee_id"    BIGINT    NOT NULL,
+  "instructor_id" BIGINT    NOT NULL,
+  "topic_id"      BIGINT    NOT NULL,
+  "date"          TIMESTAMP NOT NULL,
+  "is_approved"   BOOLEAN DEFAULT FALSE,
+  "is_completed"  BOOLEAN DEFAULT FALSE,
+  CONSTRAINT "sessionsTopicsFK" FOREIGN KEY ("topic_id")
+  REFERENCES "topics" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+  CONSTRAINT "sessionsCredentialsFK1" FOREIGN KEY ("trainee_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+  CONSTRAINT "sessionsCredentialsFK2" FOREIGN KEY ("instructor_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE "reports" (
+  "id"          BIGSERIAL PRIMARY KEY,
+  "session_id"  BIGINT    NOT NULL,
+  "user_id"     BIGINT    NOT NULL,
+  "title"       TEXT      NOT NULL,
+  "description" TEXT      NOT NULL,
+  "is_resolved" BOOLEAN DEFAULT FALSE,
+  "date"        TIMESTAMP NOT NULL,
+  CONSTRAINT "reportsSessionsFK" FOREIGN KEY ("session_id")
+  REFERENCES "sessions" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE,
+  CONSTRAINT "reportsCredentialsFK" FOREIGN KEY ("user_id")
+  REFERENCES "user_credentials" ("id")
+  ON DELETE NO ACTION
+  ON UPDATE CASCADE
+);
+
+CREATE TABLE "reviews" (
+  "id"         BIGSERIAL PRIMARY KEY,
+  "session_id" BIGINT           NOT NULL,
+  "trainee_id" BIGINT           NOT NULL,
   "rating"     DOUBLE PRECISION NOT NULL,
-  "title"      VARCHAR(200)     NOT NULL,
-  "comment"    TEXT DEFAULT '',
+  "title"      TEXT             NOT NULL,
+  "comment"    TEXT,
   "date"       TIMESTAMP        NOT NULL,
   CONSTRAINT "reviewPK" PRIMARY KEY ("id"),
-  CONSTRAINT "reviewLessonFK" FOREIGN KEY ("lesson_id")
-  REFERENCES "Lesson" ("id")
+  CONSTRAINT "reviewsSessionsFK" FOREIGN KEY ("session_id")
+  REFERENCES "sessions" ("id")
   ON DELETE NO ACTION
   ON UPDATE CASCADE,
-  CONSTRAINT "reviewAccountFK" FOREIGN KEY ("student_id")
-  REFERENCES "Account" ("id")
+  CONSTRAINT "reviewsCredentialsFK" FOREIGN KEY ("trainee_id")
+  REFERENCES "user_credentials" ("id")
   ON DELETE NO ACTION
   ON UPDATE CASCADE
 );
 
-CREATE TABLE "Transaction" (
-  "id"         BIGSERIAL,
-  "lesson_id"  BIGINT           NOT NULL,
-  "student_id" BIGINT           NOT NULL,
-  "price"      DOUBLE PRECISION NOT NULL,
-  "date"       TIMESTAMP        NOT NULL,
-  CONSTRAINT "transactionPK" PRIMARY KEY ("id"),
-  CONSTRAINT "transactionLessonFK" FOREIGN KEY ("lesson_id")
-  REFERENCES "Lesson" ("id")
-  ON DELETE NO ACTION
-  ON UPDATE CASCADE,
-  CONSTRAINT "transactionAccountFK" FOREIGN KEY ("student_id")
-  REFERENCES "Account" ("id")
-  ON DELETE NO ACTION
+CREATE TABLE "session_files" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "session_id" BIGINT NOT NULL,
+  "file_link" TEXT NOT NULL,
+  CONSTRAINT "sessionFilesFK" FOREIGN KEY ("session_id")
+  REFERENCES "sessions" ("id")
+  ON DELETE CASCADE
   ON UPDATE CASCADE
-);
-
-
+)
